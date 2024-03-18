5ecd95a7e1073edd26f90aa1a07072d4b3eb8d7a
Nicolas Riedel <nriedel@fi.uba.ar>
2024-03-17 21:06:24 -0300

    add change of mouse

--- src/levels/puzzles/Block/blockParts/simpleBlock.gd
+++ src/levels/puzzles/Block/blockParts/simpleBlock.gd
@@ -5,6 +5,10 @@ var actionName = "DefaultAction"
 var draggable = false
 var is_inside_dropable = false;
 
+var regularMouse = load("res://assets/mouse/Kunai.png")
+var dragMouse = load("res://assets/mouse/Drag.png")
+
+
 func _ready():
 	$Label.text = actionName
 
@@ -15,11 +19,13 @@ func setAction(action):
 func _on_area_2d_mouse_entered():
 #	if not GlobalBlockTracking.is_dragging:
 #		draggable=true;
+	Input.set_custom_mouse_cursor(dragMouse)
 	scale = Vector2(1.1, 1.1)
 	
 func _on_area_2d_mouse_exited():
 #	if not GlobalBlockTracking.is_dragging:
 #		draggable=true;
+	Input.set_custom_mouse_cursor(regularMouse)
 	scale = Vector2(1.0, 1.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
