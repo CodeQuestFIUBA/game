ab88911047aa2e5d3752cd7d227eb630bdb715cd
Nicolas Riedel <nriedel@fi.uba.ar>
2024-03-19 00:02:25 -0300

    global mouse tracking and level changes

--- src/levels/puzzles/Block/blockParts/simpleBlock.gd
+++ src/levels/puzzles/Block/blockParts/simpleBlock.gd
@@ -1,33 +1,36 @@
-extends Node2D
+extends Control
 
 var actionName = "DefaultAction"
 
 var draggable = false
 var is_inside_dropable = false;
-
-var regularMouse = load("res://assets/mouse/Kunai.png")
-var dragMouse = load("res://assets/mouse/Drag.png")
-
+var body_ref
+var offset: Vector2
+var initialPos : Vector2
 
 func _ready():
 	$Label.text = actionName
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass;
 
 func setAction(action):
 	actionName = action
 	$Label.text = action
 	
 func _on_area_2d_mouse_entered():
-#	if not GlobalBlockTracking.is_dragging:
-#		draggable=true;
-	Input.set_custom_mouse_cursor(dragMouse)
-	scale = Vector2(1.1, 1.1)
+	if not GlobalBlockTracking.is_dragging:
+		draggable=true;
+		GLOBAL.mouse_to_pointer()
+		scale = Vector2(1.1, 1.1)
 	
 func _on_area_2d_mouse_exited():
-#	if not GlobalBlockTracking.is_dragging:
-#		draggable=true;
-	Input.set_custom_mouse_cursor(regularMouse)
-	scale = Vector2(1.0, 1.0)
+	if not GlobalBlockTracking.is_dragging:
+		draggable=false;
+		GLOBAL.mouse_to_normal()
+		scale = Vector2(1.0, 1.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_drag_data(position : Vector2):
+	var data={}
+	data
