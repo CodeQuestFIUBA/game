b17ef1142fdaf3ca77c08832b43ea29a36cddf69
gonzalomartinezs <gmartinezs@fi.uba.ar>
2024-06-25 06:03:56 -0300

    Add more screens and connect with login

--- src/levels/Lobby/Home.gd
+++ src/levels/Lobby/Home.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+@onready var playerContainer = $PlayerContainer/CollisionPolygon2D;
+@onready var login = $Login;
+var containPlayer = true;
+
+func _process (delta):
+	#if (ApiService.token):
+		playerContainer.disabled = true;
+		login.visible = false;
+		
+
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	RoomEvents.room_entered.emit(self)
